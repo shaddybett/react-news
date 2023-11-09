ad56c48cb5f1b88b4a96069c52891f77e1d35a13
Shaddy <shadybett540@gmail.com>
2023-11-09 23:46:49 +0300

    fixed an error in Inner.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import HomePage from "./Components/HomePage";
 import { Route, Routes } from "react-router-dom";
-import Inner from "./Components/Inner";
+import Inner from "./Inner";
 export default function App() {
   return (
     <div>
